f940c3fff81145cfe1a5148a831da45c9accd3c1
NisseGunnar <101731311+NisseGunnar@users.noreply.github.com>
2022-03-28 11:51:16 +0200

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,9 +1,7 @@
 <?php
-
-
-
-
-
-
-
+<form action="login-confirmation-page.php" method="POST">
+  Username: <input type="text" name="username_entered"/><br><br>
+  Password:<input type="password" name="password_entered"/><br><br>
+  <input type="submit" name="submit" value="Log in"/><br><br>
+</form>
 ?>
