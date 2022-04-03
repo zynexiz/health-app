708505d4b3b40f773892b8008a28265dd9f05319
NisseGunnar <101731311+NisseGunnar@users.noreply.github.com>
2022-04-03 16:11:04 +0200

    Update healthlogin.php
    Lagt lite kommentarer om vad respektive del gör. Är inte hundra klar ännu men allt funkar :)

--- pages/healthlogin.php
+++ pages/healthlogin.php
@@ -1,51 +1,52 @@
 
 <?php 
 session_start(); 
-include 'database.php';
+include 'Här skall databasen vi använder skrivas';
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
-
+	//
 	function validate($data){
-       $data = trim($data);
-				$data = stripslashes($data);
-				$data = htmlspecialchars($data);
-				return $data;
+       	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
 	}
 
 	$username = validate($_POST['username']);
 	$password = validate($_POST['password']);
 
+	//Om antingen användarnamnet eller lösenordet inte anges innan inloggnigen får användaren ett meddelande att dessa måste fyllas i.
 	if (empty($username)) {
-		header('Location: projectindex.php?error=Username is required');
+		header('Location: projectindex.php?error=Username is required'); //Denna skall bytas ut mot korrekt länk
 	    exit();
 	}else if(empty($password)){
-        header('Location: projectindex.php?error=Password is required');
+        header('Location: projectindex.php?error=Password is required'); //Denna skall bytas ut mot korrekt länk
 	    exit();
 	}else{
-		$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+		$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'"; //Hämtar all info från kring användaren databasen.
 
-		$result = mysqli_query($conn, $sql);
+		$result = mysqli_query($conn, $sql); //
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
             if ($row['username'] === $username && $row['password'] === $password) {
             	$_SESSION['username'] = $row['username'];
             	$_SESSION['firstname'] = $row['fname'];
-							$_SESSION['lastname'] = $row['lname'];
+		$_SESSION['lastname'] = $row['lname'];
             	$_SESSION['id'] = $row['id'];
-            	header("Location: projecthome.php");
+            	header("Location: projecthome.php"); //Denna skall byttas ut mot korrekt länk.
 		        exit();
             }else{
-									header('Location: projectindex.php?error=Incorrect username or password');
+		header('Location: projectindex.php?error=Incorrect username or password'); //Denna skall bytas ut mot korrekt länk. //Om inte användaren anger rätt information får denne ett meddelande.
 		        exit();
 			}
 		}else{
-			header('Location: projectindex.php?error=Incorrect username or password');
+			header('Location: projectindex.php?error=Incorrect username or password'); //Denna skall bytas ut mot korrekt länk. //Om inte användaren anger rätt information får denne ett meddelande.
 	        exit();
 		}
 	}
 	
 }else{
-	header('Location: projectindex.php');
+	header('Location: projectindex.php'); //Denna skall bytas ut mot korrekt länk
 	exit();
 }
