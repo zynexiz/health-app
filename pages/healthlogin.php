116f7274dc640791de9c2c23d9e217290d7e5496
NisseGunnar <101731311+NisseGunnar@users.noreply.github.com>
2022-04-04 17:21:25 +0200

    Update healthlogin.php

--- pages/healthlogin.php
+++ pages/healthlogin.php
@@ -1,62 +1,50 @@
-
 <?php 
 session_start(); 
-include 'Här skall databasen vi använder skrivas';
+include 'database.php'; //denna skall bytas till korrekt databas
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
-	//
-	function validate($data){
-       	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
+		function validate($data){
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
 	}
-
-	$username = validate($_POST['username']);
-	$password = validate($_POST['password']);
-
-	//Om antingen användarnamnet eller lösenordet inte anges innan inloggnigen får användaren ett meddelande att dessa måste fyllas i.
-	if(empty($username) || (empty($password))) {
-			header('Location: projectindex.php?error=Username and password is required'); //Denna skall bytas mot rätt länk.
-			exit();
-	}else{
-		$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'"; //Hämtar all info från kring användaren databasen.
-
-		$result = mysqli_query($conn, $sql); //
-
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['username'] === $username && $row['password'] === $password) {
-            	$_SESSION['username'] = $row['username'];
-            	$_SESSION['firstname'] = $row['fname'];
-		$_SESSION['lastname'] = $row['lname'];
-            	$_SESSION['id'] = $row['id'];
-            	header("Location: projecthome.php"); //Denna skall byttas ut mot korrekt länk.
-		        exit();
-            }else{
-		header('Location: projectindex.php?error=Incorrect username or password'); //Denna skall bytas ut mot korrekt länk. //Om inte användaren anger rätt information får denne ett meddelande.
-		        exit();
-			}
-		}else{
-			header('Location: projectindex.php?error=Incorrect username or password'); //Denna skall bytas ut mot korrekt länk. //Om inte användaren anger rätt information får denne ett meddelande.
-	        exit();
-		}
-	}
+			$username = validate($_POST['username']);
+			$password = validate($_POST['password']);
 	
+			if(empty($username) || (empty($password))) {//Om användaren inte skrivit in lösen eller användarnamn/email får denne ett fel meddelande.
+				header('Location: projectindex.php?error=Username and password is required');
+				exit();
+	}else{
+				$sql = "SELECT * FROM users WHERE username='$username' OR email='$username' AND password='$password'";//
+				$result = mysqli_query($conn, $sql);//Resultatet blir förbindelsen till databasen och queryn ovanför.
+					if (mysqli_num_rows($result) > 0) {//Om antalet rader av resultatet är större än 1.
+						$row = mysqli_fetch_assoc($result);//Itirrerar över raderna i resultat till dessa tar slut.
+								$_SESSION['username'] = $row['username'];//Dessa skall byttas till korrekta rows
+								$_SESSION['firstname'] = $row['fname'];//Dessa skall byttas till korrekta rows
+								$_SESSION['lastname'] = $row['lname'];//Dessa skall byttas till korrekta rows
+								$_SESSION['id'] = $row['id'];//Dessa skall byttas till korrekta rows
+								header("Location: projecthome.php");//Om allt funkar omdirigeras användaren till dennes hemsida.
+								exit();
+          }else{
+								header('Location: projectindex.php?error=Wrong username or password');//Om användaren skrivit in ett oexisternade lösen eller andvändarnamn/email får denne ett fel meddelande.
+								exit();
+				}
+	}
 }else{
-	header('Location: projectindex.php'); //Denna skall bytas ut mot korrekt länk
+	header('Location: projectindex.php');//Denna skall bytas mot rätt länk.
 	exit();
 }
 
-<form action="projectlogin.php" method="post"> //Denna skall bytas mot korrekt länk
+<form action="projectlogin.php" method="post">//Denna skall bytas mot rätt länk
         <h2>Welcome to Project health!</h2>
-	<h3>Please enter your login</h3>
-        <?php if (isset($_GET['error'])) { ?> //Fångar upp URL:en och skriver ett meddelande till användaren.
+				<h3>Please enter your login</h3>
+        <?php if (isset($_GET['error'])) { ?>
                 <p class="error"><?php echo $_GET['error']; ?></p>
         <?php } ?>
-        <label>Username</label>
-        <input type="text" name="username" placeholder="Username..">
-	<label>Password</label>
-        <input type="password" name="password" placeholder="Password..">
+        <label>Username/e-mail</label>
+        <input type="text" name="username" placeholder="Enter your username or e-mail">
+				<label>Password</label>
+        <input type="password" name="password" placeholder="Enter your password">
         <button type="submit">Login</button>
     </form>
