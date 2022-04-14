056008439ca2919cdf3ba5f4caa8435d08ff76a3
Michael Rydén <zynex@zoik.se>
2022-04-14 22:13:12 +0200

    Config sample file

---
+++ config/config-sample.php
@@ -0,0 +1,15 @@
+<?php
+	# Connection settings for the application
+	$CONFIG = array (
+		'passwordsalt' => 'password_salt_string',
+		'dbuser' => 'username',
+		'dbpassword' => 'password',
+		'dbname' => 'database_name',
+		'dbhost' => 'localhost',
+		'dbport' => '3306',
+		'dbtableprefix' => 'ha_',
+		'logfile' => '/path/to/log',
+		'default_language' => 'en_US',
+		'default_theme' => 'light',
+	);
+?>
