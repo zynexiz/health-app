fbf6779eef572c830c63bbaaf4ad161bb20437ef
Michael Rydén <zynex@zoik.se>
2022-04-20 00:03:40 +0200

    Updated query

--- pages/login.php
+++ pages/login.php
@@ -8,16 +8,28 @@ if (!empty($_POST)) {
 	if(empty($username) || (empty($password))) {
 		$errLogin = _('Wrong username or password entered');
 	} else {
-		$dbConn = dbConnect();
-		$sql = "SELECT * FROM " . $CONFIG['dbtableprefix'] . "users WHERE (username='" . $username . "' OR email='" . $username ."') AND (passwd='" . $password . "')";
-		$result = mysqli_query($dbConn, $sql); # Resultatet blir förbindelsen till databasen och queryn ovanför.
-		if (mysqli_num_rows($result) > 0) { #Om antalet rader av resultatet är större än 1.
-			$row = mysqli_fetch_assoc($result); # Itirrerar över raderna i resultat till dessa tar slut.
+		$sql = <<<SQL
+		SELECT ha_users.uid,username,email,urole,fname,lname,sex,height,birthdate,ha_lang.lang,ha_lang.code,ha_uimode.css FROM {$CONFIG['dbtableprefix']}users
+		LEFT JOIN ha_userdata ON ha_users.uid=ha_userdata.uid
+		LEFT JOIN ha_lang ON ha_userdata.lang=ha_lang.langid
+		LEFT JOIN ha_uimode ON ha_userdata.ui_mode=ha_uimode.id
+		WHERE (ha_users.username='{$username}' OR ha_users.email='{$username}') AND  (BINARY ha_users.passwd='{$password}')
+SQL;
+		$row = dbFetch($sql);
+		# If resulting query return a valid user, fetch user data and add it to the session.
+		if ($row) {
+			$row = $row[0];
 			$_SESSION['username'] = $row['username'];
-			#$_SESSION['firstname'] = $row['fname'];
-			#$_SESSION['lastname'] = $row['lname'];
+			$_SESSION['email'] = $row['email'];
 			$_SESSION['id'] = (int) $row['uid'];
 			$_SESSION['role'] = (int) $row['urole'];
+			$_SESSION['firstname'] = $row['fname'];
+			$_SESSION['lastname'] = $row['lname'];
+			$_SESSION['gender'] = $row['sex'];
+			$_SESSION['height'] = $row['height'];
+			$_SESSION['theme'] = $row['css'];
+			$_SESSION['lang'] = $row['code'];
+			$_SESSION['birthdate'] = $row['birthdate'];
 			header("Location: ?page=home"); # Om allt funkar omdirigeras användaren till dennes hemsida.
 		} else {
 			$errLogin = _('Wrong username or password entered');
@@ -39,20 +51,17 @@ if (!empty($_POST)) {
 		}
 	?>
 	<form id="loginForm" action="?page=login" method="post">
-		<label><h5><strong>Username/e-mail</strong></h5></label>
+		<label><h5><strong><?php echo _('Username/e-mail') ?></strong></h5></label>
+		<div class="input-group">
+			<span class="input-group-text bi bi-person-fill"></span>
+				<input class="form-control" type="text" name="username" placeholder="<?php echo _('Enter your username or e-mail..') ?>" required>
+		</div><br>
+		<label><h5><strong><?php echo _('Password') ?></strong></h5></label>
 			<div class="input-group">
-				<span class="input-group-text bi bi-person-fill"></span>
-					<input class="form-control" type="text" name="username" placeholder="Enter your username or e-mail.." required>
+				<span class="input-group-text bi bi-key-fill"></span>
+					<input class="form-control" type="password" name="password" placeholder="<?php echo _('Enter your password..') ?>" required>
 			</div>
 			<br>
-
-			<label><h5><strong>Password</strong></h5></label>
-				<div class="input-group">
-					<span class="input-group-text bi bi-key-fill"></span>
-						<input class="form-control" type="password" name="password" placeholder="Enter your password.." required>
-				</div>
-				<br>
-			<button class="btn btn-outline-primary" type="submit">Login</button>
-		</form>
+		<button class="btn btn-outline-primary" type="submit"><?php echo _('Login') ?></button>
+	</form>
 </div>
-
