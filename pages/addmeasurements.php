5b3bbb7dae6662493661f6202addcaab430a085c
Michael Rydén <zynex@zoik.se>
2022-04-26 16:40:41 +0200

    Updated script

--- pages/addmeasurements.php
+++ pages/addmeasurements.php
@@ -7,36 +7,25 @@
 	foreach ($data as $cat) {
 		$list[$cat['category']][] = $cat;
 	}
-var_dump($list);
-	$selectedCat = isset($_POST['cat']) ? $_POST['cat'] : $cat[0]['name'];
 
+	$tabList = '<ul class="nav nav-tabs" id="myTab" role="tablist">';
+	$tabContent = '<div class="tab-content" id="tabContent">';
+	foreach ($list as $key=>$item) {
+		#active class for active tab
+		$tabList .= '<li class="nav-item" role="presentation"><button class="nav-link" id="'.$key.'-tab" data-bs-toggle="tab" data-bs-target="#'.$key.'" type="button" role="tab" aria-controls="'.$key.'" aria-selected="true">'.$key.'</button></li>';
+		#show active class for active tabs
+		$tabContent .= '<div class="tab-pane fade" id="'.$key.'" role="tabpanel" aria-labelledby="'.$key.'-tab">';
+		$tabContent .= '<div class="d-flex align-items-start"><div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">';
+		$tabContentInner = '<div class="tab-content" id="v-pills-tabContent">';
+		foreach ($item as $content) {
+			$tabContent .= '<button class="nav-link" id="v-pills-'.$content['type'].'-tab" data-bs-toggle="pill" data-bs-target="#v-pills-'.$content['type'].'" type="button" role="tab" aria-controls="v-pills-'.$content['type'].'" aria-selected="false">'.$content['type'].'</button>';
+			$tabContentInner .= '<div class="tab-pane fade" id="v-pills-'.$content['type'].'" role="tabpanel" aria-labelledby="v-pills-'.$content['type'].'-tab">';
+			$tabContentInner .= $content['type'];
+			$tabContentInner .= '</div>';
+		}
+		$tabContent .= '</div>'. $tabContentInner.'</div></div></div>';
+	}
 
-	echo '<ul class="nav nav-tabs" id="myTab" role="tablist">';
-	echo $tabs;
-	echo '</ul>';
+	echo $tabList .'</ul>'. $tabContent . '</div>';
 ?>
 
-<div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="Health" role="tabpanel" aria-labelledby="home-tab">
-		<div class="d-flex align-items-start">
-			<div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-				<?php
-
-				?>
-				<button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
-				<button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
-				<button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
-				<button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
-			</div>
-
-			<div class="tab-content" id="v-pills-tabContent">
-				<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"><?php var_dump($hType);?></div>
-				<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-				<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-				<div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-			</div>
-		</div>
-  </div>
-  <div class="tab-pane fade" id="Workout" role="tabpanel" aria-labelledby="profile-tab">Tab 2</div>
-  <div class="tab-pane fade" id="Food" role="tabpanel" aria-labelledby="contact-tab">Tab 3</div>
-</div>
