201f6589e2f7fdc9ceb7dcda69a9550b33bbef44
Michael Rydén <zynex@zoik.se>
2022-05-05 12:34:27 +0200

    Added insert previous goals

--- pages/usergoals.php
+++ pages/usergoals.php
@@ -1,8 +1,8 @@
 <?php
-$gt =  dbFetch("SELECT * FROM ha_goaltype;");
-//var_dump($gt);
+$gt =  dbFetch("SELECT * FROM ".DBPREFIX."goaltype;");
+
 if ($_POST) {
-	$sql = "REPLACE INTO ha_goals (gtid, uid, goal) VALUES ";
+	$sql = "REPLACE INTO ".DBPREFIX."goals (gtid, uid, goal) VALUES ";
 	//var_dump($_POST);
 	foreach ($gt as $value) {
 		if($_POST[$value['name']]) {
@@ -13,10 +13,6 @@ if ($_POST) {
 	//Substring returns part of a string, in this case the first position of the string and the whole length of it and also removes the coma.
 	$sql = substr($sql,0,strlen($sql)-1);
 
-
-
-
-
 	//If the database query is true it will echo "Success", else it will do something.
 	if (dbQuery($sql) === true ) {
 		echo '<div class="alert alert-success"><strong>'._('Your goals are set!').'</strong></div>';
@@ -24,7 +20,6 @@ if ($_POST) {
 		echo '<div class="alert alert-danger"><strong>'._('Something went wrong').'</strong></div>';
 	}
 }
-//die;
 
 //Array with icons for each activity. Weight = Key, bi = Value.
 $iconArray = array(
@@ -36,29 +31,27 @@ $iconArray = array(
 	'Powernap' => 'bi-clock',
 	'Calories' => 'bi-activity',
 );
-//die;
-?>
-
-
-
-<div class='container-fluid'>
-<h3>
-<span class="bi bi-award-fill"></span>
-Enter your goals</h3>
-<form id="GoalForm" action="?page=usergoals" method="post">
-<?php
-//Itirates over every row(activity) from the DB and prints the name.
-foreach ($gt as $row) {
-	echo "<label class='formlabel'>" . _($row['name']) . "</label>";
-	echo "<div class='input-group'>";
-	echo "<span class='input-group-text bi ". $iconArray[$row['name']]."'></span>";
-	echo "<input class='form-control' type='number' name='".$row['name']."' placeholder='". _('Enter') ." ".strtolower(_($row['name'])). "'></input><br>";
-	echo "</div>";
-
+$query = dbFetch("SELECT * FROM ".DBPREFIX."goals WHERE uid=".$_SESSION['id'].";");
+foreach ($query as $val) {
+	$goals[$val['gtid']] = $val['goal'];
 }
 ?>
-<br>
-<input type="submit" class="btn btn-success" value="<?php echo _('Save goals');?>">
 
+<div class='container-fluid'>
+<h3><?php echo _('Enter your goals')?></h3>
+
+<form id="GoalForm" action="?page=usergoals" method="post">
+	<?php
+	//Itirates over every row(activity) from the DB and prints the name.
+	foreach ($gt as $row) {
+		echo "<label class='formlabel'>" . _($row['name']) . "</label>";
+		echo "<div class='input-group'>";
+		echo "<span class='input-group-text bi ". $iconArray[$row['name']]."'></span>";
+		echo "<input class='form-control' type='number' name='".$row['name']."' value='". $goals[$row['gtid']] ."' placeholder='". _('Enter') ." ".strtolower(_($row['name'])). "'></input><br>";
+		echo "</div>";
+	}
+	?>
+	<br>
+	<input type="submit" class="btn btn-success" value="<?php echo _('Save goals');?>">
 </form>
 </div>
