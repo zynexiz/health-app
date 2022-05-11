23c39d9dfeb208bbd84058867a683ea7a72f95c0
Michael Rydén <zynex@zoik.se>
2022-05-11 12:14:57 +0200

    Fixed redirect bug

--- pages/logout.php
+++ pages/logout.php
@@ -4,5 +4,5 @@
 	session_write_close();
 	setcookie(session_name(),'',0,'/');
 	session_regenerate_id(true);
-	header("Location: ?page=home");
+	header("Location: ".dirname($_SERVER['SCRIPT_NAME']));
 ?>
