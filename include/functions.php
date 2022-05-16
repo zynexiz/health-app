7988a3284d84354480335922ea30edb4ef24d11b
Michael Rydén <zynex@zoik.se>
2022-05-16 13:56:38 +0200

    Updated language

--- include/functions.php
+++ include/functions.php
@@ -8,7 +8,7 @@ function setLanguage($language) {
 	putenv("LANG=".$language);
 	putenv("LANGUAGE=".$language);
 	setlocale(LC_ALL, $language);
-	$domain = "messages2";
+	$domain = "messages3";
 	$ret = bindtextdomain($domain, "./assets/lang");
 	bind_textdomain_codeset($domain, 'UTF-8');
 	textdomain($domain);
